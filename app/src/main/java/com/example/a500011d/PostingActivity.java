b8fed6fac20aa607cbb535436f17c0029794ea77
chuamingkai <chuamingkai@gmail.com>
2020-12-02 16:02:09 +0800

    Complete posting page functionality TODO: proper intent redirection

--- app/src/main/java/com/example/a500011d/PostingActivity.java
+++ app/src/main/java/com/example/a500011d/PostingActivity.java
@@ -3,34 +3,50 @@ package com.example.a500011d;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.IOException;
 
 public class PostingActivity extends AppCompatActivity {
     String[] status = {ItemEntry.Status.LOST.toString(), ItemEntry.Status.FOUND.toString()};
     ItemEntry.Status statusSelected;
     String username;
-    String imagePath; //TODO: image upload
+    String imagePath;
+
     EditText editTextItem;
     EditText editTextLocation;
     EditText editTextDescription;
+    ImageView imageViewSelected;
 
+    Button buttonSelectImage;
     Button buttonPost;
 
     DatabaseReference mRootDatabaseRef;
     DatabaseReference mNodeRefItem;
+
+    final StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+    final static int REQUEST_IMAGE_GET = 2000;
+    final static String KEY_PATH = "Image";
+    final static String KEY_NAME = "Name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,11 +75,26 @@ public class PostingActivity extends AppCompatActivity {
         editTextItem = findViewById(R.id.editTextItem);
         editTextLocation = findViewById(R.id.editTextLocation);
         editTextDescription = findViewById(R.id.editTextDescription);
+        imageViewSelected = findViewById(R.id.imageViewSelected);
 
+        buttonSelectImage = findViewById(R.id.buttonSelectImage);
         buttonPost = findViewById(R.id.buttonPost);
 
         mRootDatabaseRef = FirebaseDatabase.getInstance().getReference();
         mNodeRefItem = mRootDatabaseRef.child(getString(R.string.item_node_key));
+
+        buttonSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_IMAGE_GET);
+                }
+
+            }
+        });
 
         buttonPost.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,5 +127,27 @@ public class PostingActivity extends AppCompatActivity {
 
         });
 
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
+            Uri fullPhotoUri = data.getData();
+            try {
+                String filename = "/backgrounds/" + FireBaseUtils.getFileName(PostingActivity.this, fullPhotoUri);
+                imagePath = filename;
+                StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+                StorageReference imageRef = storageRef.child(filename);
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), fullPhotoUri);
+                FireBaseUtils.uploadImageToStorage(PostingActivity.this, imageRef, bitmap);
+            } catch (IOException e) {
+                Log.e("uploadimage", "IOException getting image");
+                e.printStackTrace();
+            }
+            imageViewSelected.setImageURI(fullPhotoUri);
+        }
+        else {
+            Toast.makeText(PostingActivity.this, R.string.file_not_found, Toast.LENGTH_LONG);
+        }
     }
 }
